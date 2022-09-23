c399e19d89b2d996c7e5268aa88ac2239016d528
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-23 20:33:46 +0800

    w2

--- 1 copy 3.py
+++ 1 copy 3.py
@@ -1,4 +1,14 @@
 drink = input("飲料：")
-cups = input("杯數：")
+cups = int(input("杯數："))
+if drink == "A" :
+    total = 50 * cups
+if drink == "B" :
+    total = 60 * cups
+if drink == "C" :
+    total = 70 * cups
+
+
+print(f'{total:,.0f}元')
+
 
 
