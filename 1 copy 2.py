7c062fb038774fe06599172696c6b0df40947111
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-26 14:44:35 +0800

    remove:

--- 1 copy 2.py
+++
@@ -1,7 +0,0 @@
-a = int(input("公克:"))
-
-total = 0.65 * a
-if a >= 200 : 
-  total = total * 0.9
-print(int(total),"元")
-
--- 1 copy.py
+++
@@ -1,9 +0,0 @@
-a = int(input("杯數:"))
-運費= a
-total = (55 * a ) + a
-if a >= 4 : 
-  total = (55*a) +0
-else :
-    total = (55*a) +60
-print(f'{total:,.0f}元')
-
