8489873223c556383fd684848feafba02dfdb6cd
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-27 16:47:59 +0800

    0927

--- 111-1-Python-5.py
+++ 111-1-Python-5.py
@@ -1,6 +1,13 @@
-class = str(input("請輸入系別："))
-Chinese = int(input("請輸入分數"))
-English = int(input("請輸入分數"))
-Math = int(input("請輸入分數"))
-
-
+a = str(input("請輸入系別："))
+Chinese = int(input("請輸入國文分數"))
+English = int(input("請輸入英文分數"))
+Math = int(input("請輸入數學分數"))
+if a == "1" :
+    total = Chinese * 1.5 +  English * 2 + Math * 1.5
+elif a =="2":
+    total = Chinese * 2 + English * 1.5 + Math * 1
+elif a == "3":
+    total = Chinese * 1.5 + English * 1.5 + Math * 1
+elif a == "4" :
+    total = Chinese * 1 + English * 1 + Math * 2
+print(total)
