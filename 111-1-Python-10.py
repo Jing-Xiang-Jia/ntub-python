1860bde9b55e9cbf0adc8cff65ab6df305357731
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-28 10:58:34 +0800

    0928

--- 111-1-Python-10.py
+++ 111-1-Python-10.py
@@ -1,6 +1,8 @@
 set_number = int(input("請輸入座位號碼1~20:"))
-if set_number > 20:
-    break
-print("看題目啊～～～")
-    
 people = int(input("請輸入人數:"))
+if set_number % 3 != 0:
+    total = 500 * people
+else:
+     total = 400 * people
+print(total)
+    
