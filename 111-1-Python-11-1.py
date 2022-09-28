b28143736ce51e4c8717b29453ec9f431de8fbc6
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-28 17:57:24 +0800

    0928

--- 111-1-Python-11-1.py
+++ 111-1-Python-11-1.py
@@ -1,9 +1,11 @@
-set_area = input("請輸入座位區:")
+set_area = str(input("請輸入座位區:"))
 people = int(input("請輸入人數:"))
 area = ["A","C","E","F","K"]
 for data in area:
-    if area == data:
+    if data == set_area:
         total = 350 * people
+        print(f"共{total:,.0f}元")
     else:
         total = 550 * people
-print(f"共{total:,.0f}元")
+        print(f"共{total:,.0f}元")
+        
