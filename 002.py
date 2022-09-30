440d675c8bc84fa6558f6ec498b4d0648246c902
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-30 19:12:29 +0800

    0930

--- 002.py
+++ 002.py
@@ -1,6 +1,18 @@
 id = input("身分證字號：")
 m = id[-1]
+
+#判斷1
 if m=="0" or m=="2" or m=="4" :
+    print("中獎")
+else:
+    print("沒中獎")
+#判斷2
+if m in ("0","2","4"):
+    print("中獎")
+else:
+    print("沒中獎")
+#判斷3
+if m in ["0","2","4"]:
     print("中獎")
 else:
     print("沒中獎")
--- 003.py
+++ 003.py
@@ -1,9 +1,26 @@
+#判斷4
 id = input("身分證字號：")
 list=["0","2","4"]
-
 if id[-1] in list :
     print("中獎")
 else:
     print("沒中獎")
+#判斷5
+id = input("身分證字號：")
+list=("0","2","4")
+if id[-1] in list :
+    print("中獎")
+else:
+    print("沒中獎")
+
+#turple型別
+a = ("2","4","6","8")
+print(a)
+print(type(a))
+
+#list型別
+a = ["2","4","6","8"]
+print(a)
+print(type(a))
 
 
