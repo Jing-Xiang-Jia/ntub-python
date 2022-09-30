f20de91ea82b08e1c403d0c42f32d668353fc564
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-30 15:36:15 +0800

    0930

---
+++ 111-1-Python-15.py
@@ -0,0 +1,6 @@
+hight = float(input("請輸入身高："))
+weight = float(input("請輸入體重："))
+if hight >= 170 and hight <= 185 and weight >= 55 and weight <= 65:
+    print("符合相親標準")
+else:
+    print("不符合相親標準")
