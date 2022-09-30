d0e71766dcbf5cd4d6c2bf49af90dad07eec636e
Jing-Xiang-Jia <johnson880225@gmail.com>
2022-09-30 19:03:34 +0800

    0930

---
+++ 002.py
@@ -0,0 +1,9 @@
+id = input("身分證字號：")
+m = id[-1]
+if m=="0" or m=="2" or m=="4" :
+    print("中獎")
+else:
+    print("沒中獎")
+
+
+
---
+++ 003.py
@@ -0,0 +1,9 @@
+id = input("身分證字號：")
+list=["0","2","4"]
+
+if id[-1] in list :
+    print("中獎")
+else:
+    print("沒中獎")
+
+
